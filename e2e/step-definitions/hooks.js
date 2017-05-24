447cf81aad7c66fd298d252e5c885866c61482bd
Sarthak Mehta <sarthak_mehta@epam.com>
2017-05-24 11:43:00 +0530

    Add files via upload

--- e2e/step-definitions/hooks.js
+++ e2e/step-definitions/hooks.js
@@ -4,7 +4,10 @@ module.exports = function () {
     this.setDefaultTimeout(60000);
     
     this.After(function () {
-        return browser.executeScript('window.localStorage.clear();');
+        return browser.executeScript('window.localStorage.clear();')
+        .then(function() {
+            browser.manage().deleteAllCookies();
+        });
     });
 
 };
